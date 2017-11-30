cc22c37eba0ce4048e48ef25d37478bda4c20315
judenki <judenkirobotics@gmail.com>
2017-11-29 20:00:08 -0500

    Mild debugging.  Removed build warnings.

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/PushbotAutoDriveByEncoder_Linear.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/PushbotAutoDriveByEncoder_Linear.java
@@ -61,7 +61,6 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-
 @Autonomous(name="Pushbot: Auto Drive By Encoder", group="Pushbot")
 @Disabled
 public class PushbotAutoDriveByEncoder_Linear extends LinearOpMode {
